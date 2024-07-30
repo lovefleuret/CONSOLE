9e120a84acfc02dc7a06ae734f17cbc542d2ee0e
fleuret <614832056.com>
2024-07-30 12:16:09 -0400

    添加debug宏配置

--- include/debug.h
+++ include/debug.h
@@ -1,7 +1,11 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#ifdef CONFIG_DEBUG
 #define DEBUG printf("------------%d-----------%s---------\n",  __LINE__, __FUNCTION__);
-
-
+#else
+#define DEBUG do{}while(0);
 #endif
+
+
+#endif //? DEBUG_H

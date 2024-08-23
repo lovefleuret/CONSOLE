174c395c36f6f4f2bb8c8916883d755863ff3041
FlruretC++ <614832056@qq.com>
2024-08-23 02:33:34 +0000

    超声波驱动程序

--- include/debug.h
+++ include/debug.h
@@ -2,7 +2,7 @@
 #define DEBUG_H
 
 #ifdef CONFIG_DEBUG
-#define DEBUG printf("------------%d-----------%s---------\n",  __LINE__, __FUNCTION__);
+#define DEBUG printf("-------\x1b[33m-----------------%s--------------------------------%s-------%d---\x1b[0m----\n", __FILE__, __func__, __LINE__);
 #else
 #define DEBUG do{}while(0);
 #endif
